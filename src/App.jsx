70fb84d418fe0ff59363a304daa43fd044525717
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-05-06 00:10:51 +0000

    inport du composant  Project dans App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Contact from "./components/Contacts";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
-    <div >
+    <div>
       <Header />
       <Hero />
       <About />
+      <Projects />
       <Contact />
       <Footer />
     </div>
